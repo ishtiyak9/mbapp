fc095e216b5dbbe430741ebb1bee9a7260fbd00e
ishtiyak25 <rahman.mdishtiyak95@gmail.com>
2023-04-09 02:22:12 +0600

    allow hosts

--- mbapp/settings.py
+++ mbapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '75.101.237.83']
 
 
 # Application definition
